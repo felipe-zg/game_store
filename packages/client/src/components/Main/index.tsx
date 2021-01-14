13b2856a16e35371be96eaa6393205cbd473b4b6
felipe-zg <felipe_zeba@outlook.com>
2021-01-14 14:34:30 -0300

    feat: add Main component style

--- packages/client/src/components/Main/index.tsx
+++ packages/client/src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado"
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      Typescript, Re4actJS, NextJS e styled components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor sentado de frente para um monitor com código"
+    />
+  </S.Wrapper>
 )
 
 export default Main
